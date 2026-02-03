bcbcf12f6c86f2c15b586090ca21e475e7f03ef0
jadrake75 <jadrake75@gmail.com>
2026-02-02 21:42:11 -0500

    Rework mockito use

--- src/test/java/org/javad/stamp/pdf/StampBoxTest.java
+++ src/test/java/org/javad/stamp/pdf/StampBoxTest.java
@@ -6,17 +6,24 @@ import org.javad.pdf.OutputBounds;
 import org.javad.pdf.model.PageConfiguration;
 import org.javad.pdf.model.PageConfigurations;
 import org.javad.stamp.pdf.StampBox.Bisect;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mock;
 
 import com.itextpdf.text.pdf.PdfContentByte;
 
+@ExtendWith(MockitoExtension.class)
 public class StampBoxTest {
 
 	float TOLERANCE = 0.01f;
 	private PageConfiguration configuration;
 	
-	@Before
+	@Mock
+	PdfContentByte cByte;
+	
+	@BeforeEach
 	public void setup() {
 		PageConfigurations configs = PageConfigurations.getInstance();
 		configuration = configs.getActiveConfiguration();
@@ -26,7 +33,7 @@ public class StampBoxTest {
 		
 		StampBox stamp = new StampBox(configuration);
 		stamp.setBisect(Bisect.top_left);
-		PdfContentByte cByte = mock(PdfContentByte.class);
+		//PdfContentByte cByte = mock(PdfContentByte.class);
 		OutputBounds rect = new OutputBounds(50,50,20,100);
 		stamp.drawBisect(cByte, rect);
 	}
@@ -36,7 +43,7 @@ public class StampBoxTest {
 		
 		StampBox stamp = new StampBox(configuration);
 		stamp.setBisect(Bisect.top_right);
-		PdfContentByte cByte = mock(PdfContentByte.class);
+		//PdfContentByte cByte = mock(PdfContentByte.class);
 		OutputBounds rect = new OutputBounds(50,50,20,100);
 		stamp.drawBisect(cByte, rect);
 	}
@@ -46,7 +53,7 @@ public class StampBoxTest {
 		
 		StampBox stamp = new StampBox(configuration);
 		stamp.setBisect(Bisect.vertical);
-		PdfContentByte cByte = mock(PdfContentByte.class);
+		//PdfContentByte cByte = mock(PdfContentByte.class);
 		OutputBounds rect = new OutputBounds(50,50,20,100);
 		stamp.drawBisect(cByte, rect);
 	}
